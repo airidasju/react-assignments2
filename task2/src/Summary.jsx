4ceba856cd78039cf12f2a57c45abdb2f5ba1c7f
Airidas Juska <airidas665@gmail.com>
2023-03-10 17:42:51 +0200

    fix

--- task2/src/Summary.jsx
+++ task2/src/Summary.jsx
@@ -3,15 +3,16 @@ import { useEffect, useState } from 'react';
 function Summary({ person }) {
   const [allMoney, setAllMoney] = useState(0);
 
-  // useEffect(() => {
-  //   if (person.length < 1) {
-  //     return;
-  //   } else if (person.length === 1) {
-  //     setAllMoney(person[0].balance);
-  //   } else {
-  //     setAllMoney(person.reduce((sum, obj) => sum + obj['balance'], 0));
-  //   }
-  // }, [person]);
+
+  useEffect(() => {
+    if (person === null) {
+      return;
+    } else if (person.length === 1) {
+      setAllMoney(person[0].balance);
+    } else {
+      setAllMoney(person.reduce((sum, obj) => sum + obj['balance'], 0));
+    }
+  }, [person]);
 
   if(person === null) {
     return
